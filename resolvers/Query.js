24be90e12ab09a5156e2f10bac07691c7e2cd14e
DENIS <den465341@gmail.com>
2022-08-07 21:48:18 +0300

    feat: implement filtering data logic

---
+++ resolvers/Query.js
@@ -0,0 +1,21 @@
+exports.Query = {
+  products: (parent, { filter }, {products}) => {
+    let filteredProducts = products;
+
+    if (filter) {
+        if (filter.onSale === true) {
+            filteredProducts = filteredProducts.filter(product => {
+                return product.onSale
+            })
+        }
+    }
+    return filteredProducts
+  },
+  product: (parent, { id }, {products}) => {
+    return products.find((product) => product.id === id);
+  },
+  categories: (parent, args, {categories}) => categories,
+  category: (parent, {id}, {categories}) => {
+    return categories.find((category) => category.id === id);
+  },
+};
